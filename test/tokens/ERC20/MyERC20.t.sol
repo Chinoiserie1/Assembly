ee80f8f71038602359a8ab847796b877cc37ea19
chixx <lucottejeremie@gmail.com>
2023-05-20 16:54:35 +0200

    clean repo

--- test/tokens/ERC20/MyERC20.t.sol
+++ test/tokens/ERC20/MyERC20.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../../src/tokens/MyERC20.sol";
+import "../../../src/tokens/ERC20/MyERC20.sol";
 
 contract MyERC20Test is Test {
   MyERC20 public myERC20;
