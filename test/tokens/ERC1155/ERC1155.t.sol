e1dfeae62c16c726d96f89437d52a215c772333b
chixx <lucottejeremie@gmail.com>
2023-05-21 12:22:57 +0200

    upgrade ERC1155 add test testERC1155SafeBatchTransferFromAnotherUserWithApprovalSet

--- test/tokens/ERC1155/ERC1155.t.sol
+++ test/tokens/ERC1155/ERC1155.t.sol
@@ -289,6 +289,27 @@ contract ERC1155Test is Test {
     require(balanceId2 == 50, "fail batch transfer");
   }
 
+  function testERC1155SafeBatchTransferFromAnotherUserWithApprovalSet() public {
+    testERC1155.mint(user1, 1, 100, "");
+    testERC1155.mint(user1, 2, 100, "");
+    vm.stopPrank();
+    vm.startPrank(user1);
+    testERC1155.setApprovalForAll(user2, true);
+    vm.stopPrank();
+    vm.startPrank(user2);
+    uint256[] memory ids = new uint256[](2);
+    ids[0] = 1;
+    ids[1] = 2;
+    uint256[] memory amounts = new uint256[](2);
+    amounts[0] = 20;
+    amounts[1] = 50;
+    testERC1155.safeBatchTransferFrom(user1, user2, ids, amounts, "");
+    uint256 balanceId1 = testERC1155.balanceOf(user2, 1);
+    uint256 balanceId2 = testERC1155.balanceOf(user2, 2);
+    require(balanceId1 == 20, "fail batch transfer");
+    require(balanceId2 == 50, "fail batch transfer");
+  }
+
   function testERC1155SafeBatchTransferFromFailToAddressZero() public {
     testERC1155.mint(user1, 1, 100, "");
     testERC1155.mint(user1, 2, 100, "");
