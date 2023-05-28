aeed1b32b62d5e7abd019f5044ad2464a7f5e73c
chixx <lucottejeremie@gmail.com>
2023-05-28 15:11:02 +0200

    add ERC1155Supply add fn totalSupply

--- src/tokens/ERC1155/extensions/ERC1155Supply.sol
+++ src/tokens/ERC1155/extensions/ERC1155Supply.sol
@@ -14,5 +14,17 @@ import "../ERC1155.sol";
  * @author chixx.eth
  */
 abstract contract ERC1155Supply is ERC1155 {
-  
+  mapping(uint256 => uint256) private _totalSupply;
+
+  /**
+   * @dev Total amount of tokens in with a given id.
+   */
+  function totalSupply(uint256 id) public view virtual returns (uint256) {
+    assembly {
+      mstore(0x00, id)
+      mstore(0x20, _totalSupply.slot)
+      mstore(0x00, sload(keccak256(0x00, 0x40)))
+      return(0x00, 0x20)
+    }
+  }
 }
